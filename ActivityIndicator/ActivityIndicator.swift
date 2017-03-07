44f85c3495a44f2754d1c232f53b968bc08cc0ff
Appinventiv <mansi2mittal@gmail.com>
2017-03-07 11:20:25 +0530

    no message

--- ActivityIndicator/ActivityIndicator.swift
+++ ActivityIndicator/ActivityIndicator.swift
@@ -19,25 +19,45 @@ import  UIKit
     
      let transparentView = UIView()
     
+    let container: UIView = UIView()
+    
      var isLoading = false
     
     // FUNCTION TO BE CALLED WHEN THE LOADER IS STARTED
     
     func loaderStarted(view : Any){
         
-        if isLoading == false{
+       if isLoading == false{
         
     isLoading = true
-    activityIndicator.center =  (view as AnyObject).center
+        
+    container.frame = (view as AnyObject).frame
+    container.center = (view as AnyObject).center
+    container.backgroundColor = UIColor.clear
+        
+    let loadingView: UIView = UIView()
+    loadingView.frame = CGRect( x: 0, y : 0, width : 80, height : 80)
+    loadingView.center = (view as AnyObject).center
+    loadingView.backgroundColor = UIColor.randomColor()
+    loadingView.clipsToBounds = true
+    loadingView.layer.cornerRadius = 10
+
+    activityIndicator.frame = CGRect( x : 0.0, y : 0.0, width : 40.0, height : 40.0)
     activityIndicator.hidesWhenStopped = true
     activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.whiteLarge
-    (view as AnyObject).addSubview(activityIndicator)
+    activityIndicator.color = UIColor.randomColor()
+    activityIndicator.center =  CGPoint( x : loadingView.frame.size.width / 2,
+                                             y : loadingView.frame.size.height / 2)
+
+    loadingView.addSubview(activityIndicator)
+    container.addSubview(loadingView)
+    (view as AnyObject).addSubview(container)
         
     activityIndicator.startAnimating()
         
-    transparentView.frame = CGRect(x: 0, y: 0, width: (view as AnyObject).frame.size.width, height: (view as AnyObject).frame.size.height)
+    //transparentView.frame = CGRect(x: 0, y: 0, width: (view as AnyObject).frame.size.width, height: (view as AnyObject).frame.size.height)
         
-    (view as AnyObject).addSubview(transparentView)
+   // (view as AnyObject).addSubview(transparentView)
 
     Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(stopLoader), userInfo: nil, repeats: false)
         }
@@ -55,7 +75,8 @@ import  UIKit
         if isLoading == true{
             
         activityIndicator.stopAnimating()
-        self.transparentView.removeFromSuperview()
+        //self.transparentView.removeFromSuperview()
+         self.container.removeFromSuperview()
         isLoading = false
             
         }
@@ -65,5 +86,30 @@ import  UIKit
         }
     }
     
+    
+    func showActivityIndicator(uiView: UIView) {
+        let container: UIView = UIView()
+        container.frame = uiView.frame
+        container.center = uiView.center
+        container.backgroundColor = UIColor.clear
+        
+        let loadingView: UIView = UIView()
+        loadingView.frame = CGRect( x: 0, y : 0, width : 80, height : 80)
+        loadingView.center = uiView.center
+        loadingView.backgroundColor = UIColor.randomColor()
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        
+        let actInd: UIActivityIndicatorView = UIActivityIndicatorView()
+        actInd.frame = CGRect( x : 0.0, y : 0.0, width : 40.0, height : 40.0);
+        actInd.activityIndicatorViewStyle =
+            UIActivityIndicatorViewStyle.whiteLarge
+        actInd.center = CGPoint( x : loadingView.frame.size.width / 2,
+                                 y : loadingView.frame.size.height / 2);
+        loadingView.addSubview(actInd)
+        container.addSubview(loadingView)
+        uiView.addSubview(container)
+        actInd.startAnimating()
+    }
     
 }
---
+++ ActivityIndicator/Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  RandomColor.swift
+//  ActivityIndicator
+//
+//  Created by Appinventiv on 07/03/17.
+//  Copyright © 2017 Appinventiv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func randomColor() -> UIColor {
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
+    }
+}
+
+
+
